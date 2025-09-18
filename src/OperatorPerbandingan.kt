f0cfeba3d63d6b15dabfc9c8fa7cdfadf3c34482
Wilcent64 <145772736+Wilcent64@users.noreply.github.com>
2025-09-18 20:40:48 +0700

    Operator

---
+++ src/OperatorPerbandingan.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val nilaiA = 20
+    val nilaiB = 15
+
+    val apakahlebihBesar = nilaiA > nilaiB
+    println(apakahlebihBesar)
+
+    val apakahSama = nilaiA == nilaiB
+    println(apakahSama)
+
+    val apakahTidakSama = nilaiA != nilaiB
+    println(apakahTidakSama)
+
+    println("apel" < "Kacang")
+}
